436d00600af1c76642245bdca8e9a90b64aed379
Namit Nathwani <namsnath@gmail.com>
2018-09-27 00:20:31 +0530

    Fixed location errors. Added basic check for current location (205m range)

--- lib/home-page.dart
+++ lib/home-page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 //import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:simple_permissions/simple_permissions.dart';
@@ -10,8 +12,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
   Permission permission;
-  Position pos;
+  Position position;
+  String posString = "Unknown";
+
+  double sjtDistance, ttDistance, smvDistance, gdnDistance;
+
   TextEditingController _c = new TextEditingController();
   TextEditingController _sjt = new TextEditingController();
   TextEditingController _tt = new TextEditingController();
@@ -50,11 +57,74 @@ class _HomePageState extends State<HomePage> {
     double smvDist = await Geolocator().distanceBetween(lat, lon, latSMV, lonSMV);
     double gdnDist = await Geolocator().distanceBetween(lat, lon, latGDN, lonGDN);
 
+    sjtDistance = sjtDist;
+    ttDistance = ttDist;
+    smvDistance = smvDist;
+    gdnDistance = gdnDist;
+
     _sjt.text = sjtDist.toString();
     _tt.text = ttDist.toString();
     _smv.text = smvDist.toString();
     _gdn.text = gdnDist.toString();
   }
+
+   getLastLocation() async {
+    print("Couldn't get accurate location. Check GPS.");
+    position = await Geolocator().getLastKnownPosition(LocationAccuracy.high);
+    print(position);
+    return position;
+  }
+
+  _onTimeout() async {
+    print("GPS Not Enabled.");
+    position = await Geolocator().getLastKnownPosition(LocationAccuracy.high);
+    print(position);
+    if(position == null)
+      _scaffoldKey.currentState.showSnackBar(new SnackBar(
+        content: new Text("GPS Not Enabled"),
+      ));
+    return position;
+  }
+
+  getLocation() async {
+    Future<Position> pos = Geolocator().getCurrentPosition();
+    pos.timeout(const Duration (seconds: 5), onTimeout: () => _onTimeout())
+        .catchError((err){
+          print("Handle 1");
+          setState(() {
+            posString = "Unknown";
+          });
+        });
+
+    pos.then((post) async {
+      await getDistance(post.latitude, post.longitude);
+
+      if(sjtDistance < 205)
+        posString = "SJT";
+      else if (ttDistance < 205)
+        posString = "TT";
+      else if(smvDistance < 205)
+        posString = "SMV";
+      else if(gdnDistance < 205)
+        posString = "GDN";
+      else
+        posString = "Unknown";
+
+      setState(() {
+        posString;
+      });
+      print(pos);
+      _c.text = post.toString();
+      print(post.latitude);
+      print(post.longitude);
+      getDistance(post.latitude, post.longitude);
+    }).catchError((err) =>
+      //Do something maybe?
+      print(err));
+      //print("Handle 2"));
+  }
+
+
 
   Widget build(BuildContext context) {
     final btnPermission = Padding(
@@ -115,6 +185,12 @@ class _HomePageState extends State<HomePage> {
         )
     );
 
+    final lblLoc = Text(
+      "Location: " + posString,
+      textAlign: TextAlign.center,
+      style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0)
+    );
+
     final btnUpdate = Padding(
         padding: EdgeInsets.symmetric(vertical: 16.0),
         child: Material(
@@ -130,11 +206,16 @@ class _HomePageState extends State<HomePage> {
               print(geolocationStatus);
 
               if(geolocationStatus == GeolocationStatus.granted) {
-                pos = await Geolocator().getCurrentPosition(LocationAccuracy.high);
-                _c.text = pos.toString();
-                print(pos.latitude);
-                print(pos.longitude);
-                getDistance(pos.latitude, pos.longitude);
+                //pos = await Geolocator().getCurrentPosition(LocationAccuracy.high);
+                await getLocation();
+                /*Future<Position> pos = Geolocator().getCurrentPosition();
+                pos.timeout(const Duration (seconds: 5), onTimeout: _onTimeout);
+                pos.then((post) async {
+                  _c.text = post.toString();
+                  print(post.latitude);
+                  print(post.longitude);
+                  getDistance(post.latitude, post.longitude);
+                });*/
                 //_sjt = getDistances(p1Lat, p1Lon, p2Lat, p2Lon)
               }
               else print("Not granted I guess.");
@@ -150,26 +231,32 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Home Page'),
       ),
+      key: _scaffoldKey,
+      body: new Builder(
+        builder: (BuildContext context) {
+          return new Center(
+            child: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.only(left: 50.0, right: 50.0),
+              children: <Widget>[
+                lblLoc,
+                btnPermission,
+                btnUpdate,
+                txtLocation,
+                SizedBox(height: 18.0),
+                txtSJTDistance,
+                SizedBox(height: 18.0),
+                txtTTDistance,
+                SizedBox(height: 18.0),
+                txtSMVDistance,
+                SizedBox(height: 18.0),
+                txtGDNDistance,
+              ],
+            ),
+          );
+        }
+      )
 
-      body: Center(
-          child: ListView(
-            shrinkWrap: true,
-            padding: EdgeInsets.only(left: 50.0, right: 50.0),
-            children: <Widget>[
-              btnPermission,
-              btnUpdate,
-              txtLocation,
-              SizedBox(height: 18.0),
-              txtSJTDistance,
-              SizedBox(height: 18.0),
-              txtTTDistance,
-              SizedBox(height: 18.0),
-              txtSMVDistance,
-              SizedBox(height: 18.0),
-              txtGDNDistance,
-            ],
-          ),
-      ),
 
     );
   }
